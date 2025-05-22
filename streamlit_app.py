dc34d52367ae1b0278e2a164027425059cf928a1
davrsc <cryingwhitecat@gmail.com>
2025-05-22 17:02:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ if ingredients_list:
     for ingredient in ingredients_list:
         ingredients_string += ingredient + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == ingredient, 'SEARCH_ON'].iloc[0]
-        st.subheader(ingredient + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+        # st.subheader(ingredient + ' Nutrition Information')
+        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        # sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
     insert = st.button('Submit')
     if insert:
